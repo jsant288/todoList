4b64461bd9fe80c686e401ae2d0e157883b5f609
Joel Santiago <jsant288@fiu.edu>
2021-10-29 18:01:12 +0000

    made todo list

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.scss";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+	const [list, setList] = useState([]);
+	const remove = eventRemove => {
+		const afterDelete = list.filter((items, index) => {
+			if (items !== eventRemove) {
+				return items;
+			}
+		});
+		setList(afterDelete);
+	};
+	const add = eventAdd => {
+		if (eventAdd.key == "Enter") {
+			setList([...list, eventAdd.target.value]);
+			eventAdd.target.value = "";
+		}
+	};
+	return (
+		<>
+			<input
+				type="text"
+				onKeyPress={e => {
+					add(e);
+				}}
+				input
+			/>
+			<div className="d-flex">
+				{list.map((item, index) => {
+					return (
+						<>
+							<p key={index}>{item}</p>
+							<button onClick={() => remove(item)}>
+								<i className="fas fa-trash-alt" />
+							</button>
+						</>
+					);
+				})}
+			</div>
+		</>
+	);
+};
